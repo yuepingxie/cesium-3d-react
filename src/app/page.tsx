28c86345e258f324a4d62a7d31e92bcc09fbb3eb
yuepingxie <xieyp9339@163.com>
2023-12-23 23:26:25 +0800

    state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,14 @@
 "use client"
-import { useReducer, useState } from 'react';
-import Logs from '../components/Logs/logs'
-import CesiumComponent from '@/components/CesiumComponent/CesiumMap';
+import React, { Component } from 'react'
+import './index.css'
+import Gallery from '../components/LearnState'
 
-const App = () => {
-  function clickHandler(event: any): import("react").MouseEventHandler<HTMLAnchorElement> | undefined {
-    event.preventDeafault();
-    event.stopPropagation();
-    alert('超链接发生了跳转')
-    // throw new Error('Function not implemented.');
-    return
-  }
-
-  const click = (event: { preventDefault: () => void; stopPropagation: () => void; }) => {
-    event.preventDefault(); // 取消默认行为
-    event.stopPropagation(); // 取消事件的冒泡
-
-    alert('我是App中的clickHandler！');
-  };
-
-  const [counter, setCounter] = useState(1)
-
-  function add(): void {
-    setCounter(counter + 1)
-    console.log(counter)
-
-  }
-
-  function subsract(): void {
-    setCounter(counter - 1)
-    console.log(counter)
-
-  }
-
-  return (
-    <div >
-      {/* <Logs /> */}
-      {/* <div onClick={() => { alert('div') }}>
-        <button type="button" onClick={() => { alert('123') }}>
-          点我一下
-        </button>
-        <button type="button" onClick={clickHandler}>
-          点我一下
-        </button>
-        <a href="https://www.baidu.com" onClick={click}>超链接</a>
+export default class page extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Gallery />
       </div>
-      <br />
-      <div >
-        <h1>{counter}</h1>
-        <button type="button" onClick={add}>
-          +
-        </button>
-        <button type="button" onClick={subsract}>
-          -
-        </button>
-      </div>*/}
-
-      <CesiumComponent />
-
-    </div>
-  )
+    )
+  }
 }
-
-export default App
