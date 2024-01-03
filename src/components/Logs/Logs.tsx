bb38add196441ea2255b7ffd7584991be5c8a795
yuepingxie <xieyp9339@163.com>
2024-01-03 21:28:58 +0800

    feat: no-logs

--- src/components/Logs/Logs.tsx
+++ src/components/Logs/Logs.tsx
@@ -3,13 +3,16 @@ import LogItem from './LogItem/LogItem'
 import './Logs.css'
 
 const Logs = (props: any) => {
-    const logItemDate = props.logsData.map((item: {
+    let logItemData = props.logsData.map((item: {
         id: any; date: any; desc: any; time: any
     }, index: any) => <LogItem key={item.id} date={item.date} desc={item.desc} time={item.time}
         logIndex={index} onDelLog={() => props.onDelLog(index)} />)
 
+    if (logItemData.length === 0) {
+        logItemData = <p className='no-logs'>没找到日志！</p>
+    }
     return <Card className='logs'>
-        {logItemDate}
+        {logItemData}
     </Card>
 }
 
